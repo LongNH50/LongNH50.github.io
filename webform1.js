0e90caea1047f352aa14e2bcf531507e9dcde326
Hoang Long <hoanglong210897@gmail.com>
2023-01-24 00:44:32 -0600

    Web Form Assignment

---
+++ webform1.js
@@ -0,0 +1,19 @@
+window.onload = pageload;
+
+function pageload() {
+    const EMAIL_REGEX = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,6}$/;
+
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    var submit = document.getElementById("submit");
+    submit.onclick( function() {
+        // if (email.value.match(EMAIL_REGEX)) {
+            alert("ok");
+            // console.log
+        // }
+    });
+
+
+
+
+}
